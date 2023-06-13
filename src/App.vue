3889583f3dfbcf0d08c9a4f926540ba6bff0600f
user1 <user@email.com>
2023-06-13 18:55:26 +0200

    Jumbotron fatto

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,7 @@ export default {
   <AppFooter />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -52,18 +52,30 @@ export default {
 
 <template>
     <header>
-        <div class="logo">
-            <a href="/" class="brand">
-                <img src="../assets/images/avada-bakery-logo.png" alt="backery">
-            </a>
+        <div>
+            <div class="logo">
+                <a href="/" class="brand">
+                    <img src="../assets/images/avada-bakery-logo.png" alt="backery">
+                </a>
+            </div>
+            <nav>
+                <ul>
+                    <li v-for="link in links">
+                        <a :href="link.url" :class="{ active: link.current }">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul>
-                <li v-for="link in links">
-                    <a :href="link.url" :class="{ active: link.current }">{{ link.text }}</a>
-                </li>
-            </ul>
-        </nav>
+        <div class="perfect-bread-text-container">
+            <h4>fresh & tasty bakery every day</h4>
+            <h1>The Perfect Fresh Bread</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ducimus sint distinctio voluptatem quod.
+                Modi voluptatem impedit inventore dolorem odit?</p>
+
+            <button class="explore">
+                <a href="#">Explore our products</a>
+            </button>
+        </div>
     </header>
 </template>
 
@@ -72,6 +84,45 @@ export default {
 @use '../styles/partials/mixins' as *;
 
 header {
+    padding: 0 2%;
+    height: 100vh;
+    background-image: url(../assets/images/hero-header-bakery.jpg);
+    background-size: cover;
+}
+
+.perfect-bread-text-container {
+    padding-top: 100px;
+    width: 30vw;
+    height: 30vh;
+    margin-left: 150px;
+
+    h4 {
+        color: $secondary-color;
+        font-size: 11px;
+        text-transform: uppercase;
+        font-weight: 100;
+    }
+
+    h1 {
+        color: $secondary-color;
+        font-size: 50px;
+        line-height: 60px;
+        margin: 30px 0;
+    }
+
+    p {
+        color: black;
+        font-size: 1.2rem;
+        line-height: 30px;
+
+    }
+
+    button {
+        margin: 40px 0;
+    }
+}
+
+header :first-child {
     @include my-display-flex-1 ();
     align-items: center;
 
@@ -93,15 +144,14 @@ header {
             @include center ();
 
             li {
-                margin: 0 3%;
-
-
-
+                margin: 0 2%;
 
                 a {
-                    color: black;
+                    color: $secondary-color;
                     font-weight: 600;
                     font-size: 12px;
+                    text-transform: uppercase;
+
                 }
 
                 &:active,
